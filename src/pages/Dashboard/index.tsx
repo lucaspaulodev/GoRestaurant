7701149b8fb6eaabf42c49cd0126384050357196
Lucas Paulo <lucaspaulodev@gmail.com>
2020-08-31 15:42:08 -0300

    List foods lists

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,11 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get('/foods');
+
+      const menu = response.data;
+
+      setFoods(menu);
     }
 
     loadFoods();
